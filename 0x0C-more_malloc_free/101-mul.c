73e892886ef4a24c41c4ebb629d9be6087acb9bb
Maurice-Jah <mauricejahwizwid@gmail.com>
2022-05-09 20:32:37 -0700

    change code

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,12 +3,132 @@
 #include <stdlib.h>
 
 /**
- * Error - prints Error str
+ * add - addition of parcial products
  *
- * Return: Error and new line
+ * @p: parcial product
+ *
+ * @mg: big malloc
+ *
+ * @i: size of big malloc - 1
+ *
+ * @tp: size of parcial product - 1
+ *
+ * Return: 1 all digits 0 otherwise
  */
 
-void Error(void)
+void add(char *p, char *mg, int i, int tp)
+{
+	int pos = tp, a = 0, b = 0, car = 0, res = 0;
+	int cont = tp, aux = 0;
+
+	for (; cont >=  0; cont--, i--, pos--)
+	{
+		a = *(p + pos) - '0';
+		b = *(mg + i) - '0';
+		res = (a + b) + car;
+		car = res / 10;
+		res = (res % 10) + '0';
+		*(mg + i) = res;
+	}
+	res = 0;
+	for (aux = i - 1; aux >= 0; aux--)
+	{
+		a = *(mg + aux) - '0';
+		a += a + car;
+		car = a / 10;
+		a = (a % 10) + '0';
+		*(mg + aux) = a;
+	}
+}
+
+/**
+ * setiar - fill the pointer with 0
+ *
+ * @s: pointer
+ *
+ * @a: size of pointer
+ *
+ * Return 0
+ */
+
+void setiar(char *s, int a)
+{
+	int i;
+
+	for (i = 0; i < a; i++)
+		s[i] = '0';
+}
+
+/**
+ * parcial - give the parcial malloc
+ *
+ * @a: size one
+ *
+ * Return: parcial malloc
+ */
+
+char *parcial(int a)
+{
+	int i;
+	char *p;
+
+	p = malloc(a);
+	if (!p)
+		return (NULL);
+	for (i = 0; i < a; i++)
+		p[i] = '0';
+	return (p);
+}
+
+/**
+ * grande - give big malloc
+ *
+ * @a: size one
+ *
+ * @b: size two
+ *
+ * Return: malloc
+ */
+
+char *grande(int a, int b)
+{
+	int i;
+	char *p;
+
+	p = malloc(a + b);
+	if (!p)
+		return (NULL);
+	for (i = 0; i < a + b; i++)
+		p[i] = '0';
+	return (p);
+}
+
+/**
+ * tama - give the size
+ *
+ * @s: string of numbers
+ *
+ * Return: size
+ */
+
+int tama(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * Perror - print Error with putchar.
+ *
+ * Return: Always 0
+ */
+
+void Perror(void)
 {
 	_putchar('E');
 	_putchar('r');
@@ -20,99 +140,130 @@ void Error(void)
 }
 
 /**
- * allDigits - checks if all the characters are digits
+ * numero - check if strings have only numbers
  *
- * @arg: the arguments of program
+ * @s: string 1
  *
- * Return: 1 all digits 0 otherwise
+ * @a: string 2
+ *
+ * Return: Always 1 if not or 0 if only has number
  */
 
-int allDigits(char **arg)
+void numero(char *s, char *a)
 {
-	int i, j;
+	int i = 0, aux = 0;
 
-	for (i = 1; i <= 2; i++)
-		for (j = 0; arg[i][j]; j++)
-		{
-			if (arg[i][j] < '0' || arg[i][j] > '9')
-				return (0);
-		}
-	return (1);
-}
-
-/**
- * _calloc- initializes memory spaces with zero
- *
- * @nmemb: string 1
- *
- * @size: string 2, concatenated to 1
- *
- * Return: pointer to the concatenated string
- */
-
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
-	unsigned int i;
-	char *newArray;
-
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-	newArray = malloc(nmemb * size);
-	if (newArray == NULL)
-		return (NULL);
-	for (i = 0; i < (nmemb * size); i++)
-		*(newArray + i) = 0;
-	return (newArray);
-}
-
-/**
- * main- multiplies 2 positive numbers
- *
- * @argc: counter of arguments
- *
- * @argv: vector of arguments
- *
- * Return: ans or Error
- */
-
-int main(int argc, char **argv)
-{
-	int i, j, carry, len, len_s1 = 0, len_s2 = 0;
-	char *s1 = *(argv + 1), *s2 = *(argv + 2);
-	int *a, *b, *ans;
-
-	if (argc != 3 || allDigits(argv) != 1)
-		Error();
-	if (*s1 == '0' || *s2 == '0')
-		_putchar('0');
-	while (*(*(argv + 1) + len_s1))
-		len_s1++;
-	while (*(*(argv + 2) + len_s2))
-		len_s2++;
-	len = len_s1 + len_s2 + 1;
-	a = (int *) malloc(len_s1 * sizeof(int));
-	b = (int *) malloc(len_s2 * sizeof(int));
-	ans = _calloc(len, sizeof(int));
-	if (a == NULL || b == NULL || ans == NULL)
-		Error();
-	for (i = len_s1 - 1, j = 0; i >= 0; i--, j++)
-		*(a + j) = *(s1 + i) - '0';
-	for (i = len_s2 - 1, j = 0; i >= 0; i--, j++)
-		*(b + j) = *(s2 + i) - '0';
-	for (i = 0; i < len_s2; i++)
-		for (j = 0; j < len_s1; j++)
-			*(ans + i + j) = *(ans + i + j) + *(b + i) * *(a + j);
-	for (i = 0; i < len_s1 + len_s2; i++)
+	while (s[i] != '\0')
 	{
-		carry = *(ans + i) / 10, *(ans + i) = *(ans + i) % 10;
-		*(ans + i + 1) = *(ans + i + 1) + carry;
-	}
-	for (i = len_s1 + len_s2; i >= 0; i--)
-		if (*(ans + i) > 0)
+		if (!(s[i] >= '0' && s[i] <= '9'))
+		{
+			aux = 1;
 			break;
-	for (; i >= 0; i--)
-		_putchar(*(ans + i) + '0');
+		}
+		i++;
+	}
+	i = 0;
+	while (a[i] != '\0')
+	{
+		if (!(a[i] >= '0' && a[i] <= '9'))
+		{
+			aux = 1;
+			break;
+		}
+		i++;
+	}
+	if (aux == 1)
+		Perror();
+	if (*s == '0' || *a == '0')
+	{
+		_putchar('0');
+		_putchar('\n');
+		exit(0);
+	}
+}
+
+/**
+ * impresion - print the pointer
+ *
+ * @s: string
+ *
+ * @a: size of string
+ *
+ * Return: size
+ */
+
+void impresion(char *s, int a)
+{
+	int i;
+
+	for (i = 0; i < a; i++)
+	{
+		if (!(s[0] == '0' && i == 0))
+			_putchar(s[i]);
+	}
 	_putchar('\n');
-	free(a), free(b), free(ans);
+}
+
+/**
+ * main - check the code for Holberton School students
+ *
+ * @argv: array of pointers store the arguments
+ *
+ * @argc: quantity of arguments
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int t1, t2, t3, a = 0, b = 0, j = 0, aux = 0, car, g, ind;
+	char *mg, *p, *size1, *size2;
+
+	if (argc != 3)
+		Perror();
+	else
+	{
+		numero(argv[1], argv[2]);
+		t1 = tama(argv[1]);
+		t2 = tama(argv[2]);
+		ind = t1 + t2;
+		mg = grande(t1, t2);
+		if (t1 >= t2)
+		{
+			size1 = argv[1];
+			size2 = argv[2];
+			b = t1 - 1;
+			a = t2 - 1;
+			t3 = t1 + 1;
+		}
+		else
+		{
+			size1 = argv[2];
+			size2 = argv[1];
+			b = t2 - 1;
+			a = t1 - 1;
+			t3 = t2 + 1;
+		}
+		g = b;
+		p = parcial(t3);
+		j = b + 1;
+		for (; a >= 0; a--, ind--)
+		{
+			car = 0;
+			b = g;
+			j = g + 1;
+			setiar(p, t3);
+			for (; b >= 0; b--, j--)
+			{
+				aux = ((size2[a] - '0') * (size1[b] - '0'));
+				aux = aux + car;
+				p[j] = ((aux % 10) + '0');
+				car = aux / 10;
+			}
+			p[j] = (car + '0');
+			add(p, mg, (ind - 1), (t3 - 1));
+		}
+		impresion(mg, (t1 + t2));
+	}
 	return (0);
 }
