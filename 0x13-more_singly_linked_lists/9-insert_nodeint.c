71894c12f596537e405004f9f33754e10129f383
Maurice-Jah <mauricejahwizwid@gmail.com>
2022-05-26 19:11:39 -0700

    fix error

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,51 +1,50 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index -  insert a node in a idx position
- * @head: pointer to head in function
- * @idx: index or position to be added
- * @n: node's value
+ * insert_nodeint_at_index - adds a new node at a given position in a list
+ * @head: linked list of type listint_t
+ * @idx: position to add element at
+ * @n: element to add to list
  *
- * Return: nth node data
+ * Return: address of new node, or NULL if function fails
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int nodes;
-	listint_t *node_index = *head;
-	listint_t *new_node, *node_after;
+	listint_t *new, *current;
+	unsigned int c_i = 0;
 
-	if (head == NULL || *head == NULL)/*Ask if there is not input*/
+	if (head == NULL)
 		return (NULL);
 
-	for (nodes = 0; node_index != NULL; nodes++)/*Count the num of nodes*/
-		node_index = node_index->next;
-	if (idx > (nodes + 1))/*Allows insert until after-last node*/
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
 		return (NULL);
 
-	node_index = *head;
-
-	new_node = malloc(sizeof(listint_t));/*allocate and fill the new node*/
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
+	new->n = n;
 
 	if (idx == 0)
 	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
+		new->next = *head;
+		*head = new;
+		return (new);
 	}
-	else
-	{
-		node_after = *head;
-		for (nodes = 0; nodes < (idx - 1); nodes++)
-			node_index = node_index->next;
-		for (nodes = 0; nodes < idx; nodes++)
-			node_after = node_after->next;
-		node_index->next = new_node;
-		new_node->next = node_after;
-		return (new_node);
 
+	current = *head;
+
+	while (c_i < idx - 1)
+	{
+		if (current == NULL)
+		{
+			free(new);
+			return (NULL);
+		}
+		current = current->next;
+		c_i++;
 	}
+	new->next = current->next;
+	current->next = new;
+
+	return (new);
 }
