28db60bf962dfc6eae71baa9799a8392bced948a
Maurice-Jah <mauricejahwizwid@gmail.com>
2022-05-30 17:15:14 -0700

    fix eror

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -16,12 +16,12 @@ void print_binary(unsigned long int n)
 		_putchar('0');
 	temp = n;
 
-	for (j = 0; temp != 0; j++)/*Ignoring left-zeros*/
+	for (j = 0; temp != 0; j++) /*Ignoring left-zeros*/
 		temp = temp >> 1;
 
 	for (i = j - 1; i >= 0; i--)
 	{
-		mask = 1 << i;/*Creating a mask for extract the digit*/
+		mask = 1 << i; /*Creating a mask for extract the digit*/
 		if ((n & mask) != 0)
 			_putchar('1');
 		else
