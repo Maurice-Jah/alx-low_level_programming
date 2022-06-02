9f52c00c7cf6ad66b37ed8a31aaa745006dd7e44
Maurice-Jah <mauricejahwizwid@gmail.com>
2022-06-01 17:48:06 -0700

    Rewrite code

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,51 +1,65 @@
 #include "main.h"
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 /**
- * main - copy 1 file to another
- * @argc: argument counter
- * @argv: argument vector
+ * main - Program that copies the content of a file into another file
+ * @argv: input arguments
+ * @argc: quantity of arguments
  *
- * Return: 2 success, otherwise 0
+ * Return: nothing
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int file_from, file_to;
-	unsigned long int size;
-	char *buffer;
+	int fd, fd_to;
+	int check_in, check_out;
+	char buff[1024];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
+			exit(97);
 	}
-	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1)
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[0]);
-		exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+				argv[1]), exit(98);
 	}
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0664);
-	if (file_to == -1)
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
-		exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]),
+			exit(99);
 	}
-	buffer = malloc(1024);
-	while ((size = read(file_from, buffer, sizeof(buffer))) != 0)
-		write(file_to, buffer, size);
-	if (close(file_from) == -1)
+	check_in = check_out = 1;
+	while (check_in)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(99);
+		check_in = read(fd, buff, 1024);
+		if (check_in == -1)
+			dprintf(STDERR_FILENO,
+					"Error: Can't read from file %s\n", argv[1]),
+				exit(98);
+		if (check_in > 0)
+		{
+			check_out = write(fd_to, buff, check_in);
+			if (check_out == -1)
+				dprintf(STDERR_FILENO,
+						"Error: Can't write to %s\n", argv[2]),
+					exit(99);
+		}
 	}
-	if (close(file_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-		exit(99);
-	}
-	free(buffer);
+	check_out = close(fd);
+	if (check_out == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd),
+			exit(100);
+	check_out = close(fd_to);
+	if (check_out == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to),
+			exit(100);
 	return (0);
 }
-
