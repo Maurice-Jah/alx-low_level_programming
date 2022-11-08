8ba7bd13b790cc624793a4f096ea28dd3510534b
Maurice <mauricejahwizwid@gmail.com>
2022-11-08 04:11:16 +0100

    Add header file for advanced task

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,4 +7,16 @@
 /* Prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+
+/* advanced exercises */
+int jump_search(int *array, size_t size, int value);
+int min(int a, int b);
+int max(int a, int b);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+int rec_bin_adv(int *array, size_t left, size_t right, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
+
 #endif /* _SEARCH ALGO */
